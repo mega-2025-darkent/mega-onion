35afcebceb5a97c98df27977ee192cc76febbd5b
mega-2025-darkent <buithinhs27242@gmail.com>
2025-09-10 20:29:28 +0300

    Create mega-onion.js

---
+++ mega-onion.js
@@ -0,0 +1,22 @@
+{
+  "project": "Mega Darknet Index 2025",
+  "mirrors": [
+    {
+      "type": "clearnet",
+      "url": "https://mega2*25.top"
+    },
+    {
+      "type": "onion",
+      "url": "http://megaexample123.onion"
+    },
+    {
+      "type": "onion",
+      "url": "http://megadarknetmirror456.onion"
+    },
+    {
+      "type": "clearnet",
+      "url": "https://mega-alt2025.top"
+    }
+  ],
+  "updated": "2025-09-10"
+}
